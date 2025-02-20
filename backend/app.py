efca4cb798ba6c7781a60e08f0008600c7186b14
Avraham Niazov <aviniazov7@icloud.com>
2025-02-20 15:26:15 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,12 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from backend.routes.auth_routes import users_blueprint  # ← ייבוא ה-Blueprint
+from backend.routes.auth_routes import users_blueprint  
 
-# טוען משתני סביבה מקובץ .env
 load_dotenv()
-
 app = Flask(__name__)
 
-# הגדרת SECRET_KEY מתוך משתני סביבה
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-
-# רישום ה-Blueprint
 app.register_blueprint(users_blueprint, url_prefix="/auth")
 
 if __name__ == "__main__":
